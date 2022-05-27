02b4303fcc3e05642c41e8ecc4b49b481eea0628
lee-jee-hyeon <jahhyn@gmail.com>
2022-05-27 17:00:10 +0900

    components

---
+++ project1/src/components/TestCompo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <slot />
+    
+  </div>
+</template>
+
+<script lang="js">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name : 'TestCompo',
+
+  setup()
+  {
+
+  }
+})
+</script>
+
