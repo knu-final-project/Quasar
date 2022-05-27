8d8b2c91f8a41afd8f68eb3b8fda0ebd90531401
lee-jee-hyeon <jahhyn@gmail.com>
2022-05-27 16:36:44 +0900

    5.27

--- project1/src/pages/IndexPage.vue
+++ project1/src/pages/IndexPage.vue
@@ -1,10 +1,16 @@
 <template>
   <q-page class="flex flex-center">
+    /
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+    <div>
+      <h1>
+        안뇽
+      </h1>
+    </div>
   </q-page>
 </template>
 
---
+++ project1/src/pages/TestVue.vue
@@ -0,0 +1,31 @@
+<template>
+
+
+<div id="event-handling">
+  <p>{{ message }}</p>
+  <button v-on:click="reverseMessage">Reverse Message</button>
+</div>
+
+</template>  
+<script img = "ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name : "TestVue",
+  data() {
+    return {
+      message: '한글화 테스트'
+    }
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('')
+    }
+  }
+}
+
+)
+</script>
